89a6d09868d609b0510dc9e8418bb12662baa820
NZAK97 <nzak97@gmail.com>
2023-10-03 18:27:31 -0400

    Java Classes

---
+++ Classes made on Oct 3rd/ActiveGame.java
@@ -0,0 +1,17 @@
+
+public class ActiveGame {
+	
+	private int gameID;
+	
+	public ActiveGame(int id) {
+		this.gameID = id;
+	}
+
+	public int getGameID() {
+		return gameID;
+	}
+
+	public void setGameID(int gameID) {
+		this.gameID = gameID;
+	}
+}
---
+++ Classes made on Oct 3rd/GameManager.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class GameManager {
+	private ArrayList<Integer> list = new ArrayList<>();
+	private ArrayList<Integer> activeGames = new ArrayList<>();
+	private ArrayList<Integer> leaderBoard = new ArrayList<>();
+	
+	private int buildLeaderboard() {
+		return 0;
+	}
+	
+	public void createGame() {
+		
+	}
+	
+	public void endGame() {
+		
+	}
+	
+	public void printActiveGames() {
+		
+	}
+	
+	public void printAllBoards() {
+		
+	}
+}
