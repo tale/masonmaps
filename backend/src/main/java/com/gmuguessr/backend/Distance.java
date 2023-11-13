8b08bdbc2b739fe6359e76ad03c4f1b39bb94bb1
Huong Bui <hbui23@gmu.edu>
2023-11-13 13:08:45 -0500

    Committing local changes before pull

--- backend/src/main/java/com/gmuguessr/backend/Distance.java
+++ backend/src/main/java/com/gmuguessr/backend/Distance.java
@@ -1,7 +1,7 @@
 package com.gmuguessr.backend;
-
 /**
- * The Distance class calculates the distance between two geographical coordinates (latitude and longitude)
+ * The Distance class calculates the distance between two geographical
+ * coordinates (latitude and longitude)
  * using the Haversine formula.
  */
 public class Distance {
@@ -12,10 +12,10 @@ public class Distance {
     /**
      * Creates a new Distance instance with image and Mapbox API coordinates.
      *
-     * @param imageLatitude        The latitude of the image's location.
-     * @param imageLongitude       The longitude of the image's location.
-     * @param mapboxApiLatitude    The latitude of the Mapbox API location.
-     * @param mapboxApiLongitude   The longitude of the Mapbox API location.
+     * @param imageLatitude      The latitude of the image's location.
+     * @param imageLongitude     The longitude of the image's location.
+     * @param mapboxApiLatitude  The latitude of the Mapbox API location.
+     * @param mapboxApiLongitude The longitude of the Mapbox API location.
      */
     public Distance(double imageLatitude, double imageLongitude, double mapboxApiLatitude, double mapboxApiLongitude) {
         this.imageCoordinates = new GeoCoordinates(imageLatitude, imageLongitude);
@@ -24,8 +24,10 @@ public class Distance {
 
     /**
      * Creates a new Distance instance with image and Mapbox API coordinates.
-     * @param imageCoordinates      The latitude and longitude of the image's location
-     * @param mapboxApiCoordinates  The latitude and longitude of the user's guess
+     * 
+     * @param imageCoordinates     The latitude and longitude of the image's
+     *                             location
+     * @param mapboxApiCoordinates The latitude and longitude of the user's guess
      */
     public Distance(GeoCoordinates imageCoordinates, GeoCoordinates mapboxApiCoordinates) {
         this.imageCoordinates = imageCoordinates;
@@ -33,7 +35,8 @@ public class Distance {
     }
 
     /**
-     * Calculates the distance between the image location and the Mapbox API location
+     * Calculates the distance between the image location and the Mapbox API
+     * location
      * using the Haversine formula.
      *
      * @return The calculated distance in kilometers.
