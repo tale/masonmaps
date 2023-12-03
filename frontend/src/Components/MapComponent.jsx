349cffd5c6925199532145bb8a6df933b3365de1
Alex Choi <achoi29@gmu.edu>
2023-12-03 12:46:33 -0500

    updated MapBox Component

--- frontend/src/Components/MapComponent.jsx
+++ frontend/src/Components/MapComponent.jsx
@@ -6,7 +6,7 @@ mapboxgl.accessToken =
   'pk.eyJ1IjoiYXNjaG9pIiwiYSI6ImNsbmtwMWdvajBrODEybHBtYXR3dmE3M3YifQ.7EtnhrpR2jJ1r4mnjCMwsQ';
 
 export default function MapComponent() {
-  const coordinatesRef = useRef(null);
+  //const coordinatesRef = useRef(null);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -27,21 +27,46 @@ export default function MapComponent() {
       if (coordinatesRef.current) {
         coordinatesRef.current.style.display = 'block';
         coordinatesRef.current.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+        console.log(`Longitude: ${lngLat.lng}, Latitude: ${lngLat.lat}`);
       }
     }
 
     marker.on('dragend', onDragEnd);
 
+    // Store the marker's longitude and latitude coordinates in a variable
+    const lngLat = marker.getLngLat();
+    // Print the marker's longitude and latitude values in the console
+    console.log(`Longitude: ${lngLat.lng}, Latitude: ${lngLat.lat}`);
+
     return () => map.remove();
   }, []);
 
   const mapStyle = {
+    display: 'flex',
+    margin: 'auto',
+    flexDirection: 'column',
+    alignItems: 'center',
     width: '400px',
     height: '300px',
   };
 
+  const coordinatesRef = {
+    background: 'rgb(255, 255, 255)',
+    color: '#fc8c03',
+    position: 'absolute',
+    bottom: '40px',
+    left: '10px',
+    padding: '5px 10px',
+    margin: '0',
+    fontSize: '11px',
+    lineHeight: '18px',
+    borderRadius: '3px',
+    display: 'none',
+  };
+
   return (
     <div>
+      <h1>I think we can add any kind of container right here... </h1>
       <div id="map" style={mapStyle}></div>
       <pre id="coordinates" ref={coordinatesRef} className="coordinates"></pre>
     </div>
