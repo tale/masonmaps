756e4ca724964a978d8d97bdcfdc26ac398350e5
delinak <77298609+delinak@users.noreply.github.com>
2023-12-03 23:15:23 -0500

    Update Homepage.jsx
    Added logo and buttons for home page

--- frontend/src/Components/Homepage.jsx
+++ frontend/src/Components/Homepage.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import './homepage.css';
+import './Homepage.css';
 import { Link } from 'react-router-dom';
 
-export default function Homepage() {
+import logo_icon from '../Assets/Logo.png';
+
+const Homepage = () => {
  return (
-    <div className="homepage">
-      <h1>GeoGuessr</h1>
-      <p>Easy to learn but really hard to master, so is really addictive!</p>
-      <Link to="/game">
-        <button>Start Game</button>
-      </Link>
-      <Link to="/LoginSignup">
-        <button>Login</button>
-      </Link>
+    <div className="homepage-container">
+    <header className="center-header">
+    <div className="underline"></div>
+        <h1>MasonMaps</h1>
+    </header>
+    <text className="text-center">
+        <p>Easy to learn but really hard to master, become an expert on George Mason's Campus!</p>
+    </text> 
+    <div className="reviews-container">
+    <text className="reviews">
+        <p> "This game helped me gain familiarity with Mason and feel at home on campus!"</p>
+        <p> Megan -Freshman, Psycology Major </p>
+    </text> 
     </div>
+    <div className="icon-container">
+        <img src={logo_icon} alt="LogoIcon" className="centered-image"/>
+    </div>
+      <Link to="/game" className="button">
+       Start Game
+      </Link>
+      <Link to="/LoginSignup" className="button">
+        Login
+      </Link>
+      </div>
  );
 }
+
+
+export default Homepage;
 
